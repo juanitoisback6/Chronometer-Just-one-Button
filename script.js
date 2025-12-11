42e32d8598b34515ed9fb680cf8369d7b8c3bdc8
Dakitidunno <juanchojuan6@hotmail.com>
2025-12-10 19:36:38 -0500

    Chronometer just Start button

---
+++ script.js
@@ -0,0 +1,51 @@
+let minShow = document.getElementById("min");
+let secShow = document.getElementById("sec");
+
+let btn = document.getElementById("btn");
+
+
+
+let min = 0;
+let sec = 0;
+
+
+
+
+btn.addEventListener("click",runChrono )
+          
+          
+
+
+function runChrono () {
+
+ let sisa = setInterval(()=>{
+
+
+sec++;
+
+if (sec === 60){
+          sec= 0;
+          min++;
+}
+
+minShow.textContent= `Minutes→ ${min}`;
+secShow.textContent= `Seconds→ ${sec}`;
+ 
+}, 1000) 
+
+return sisa
+
+}
+
+
+
+
+       
+   
+
+  
+
+ 
+
+ 
+ 
